89ca8337f66b6e337afab709df41ffb88172ebc2
srubey <scrubey@pdx.edu>
2021-01-29 15:59:20 -0800

    Refactored Graph class.  Add Point class to encapsulate x/y coordinates?

--- src/Graph.java
+++ src/Graph.java
@@ -54,23 +54,52 @@ public class Graph {
         vertices.add(vertex);
     }
 
-    //edge weight = distance between Euclidian 2D coordinates
     protected void loadEdges(){
-        //create path from each vertex...
+        //create edge from each vertex...
         for(int i = 0; i < vertices.size(); ++i){
             Vertex vertex1 = vertices.get(i);
 
             //...to each other vertex
             for(int j = 0; j < vertices.size(); ++j){
                 Vertex vertex2 = vertices.get(j);
-                long edgeWeight = calcDistance(vertex1.xcoord, vertex1.ycoord, vertex2.xcoord, vertex2.ycoord);
-
-                if(vertices.get(i) != vertices.get(j)){
-                    Edge e = new Edge(vertex2, edgeWeight);
-                    vertex1.edges.add(e);
-                }
+                addEdge(vertex1, vertex2);
             }
         }
+    }
+
+    private void addEdge(Vertex vertex1, Vertex vertex2) {
+        //only calculate edgeweight and add to vertex1's edgelist if we're dealing with two different vertices
+        boolean sameVertex = vertex1 == vertex2;
+        if(!sameVertex){
+            long edgeWeight = getDistanceBetween(vertex1, vertex2);
+            Edge edge = new Edge(vertex2, edgeWeight);
+            addEdgeToList(vertex1, edge);  //adding to vertex1's edgelist
+        }
+    }
+
+    //edge weight = distance between Euclidean 2D coordinates
+    protected long getDistanceBetween(Vertex vertex1, Vertex vertex2){
+        float x1 = vertex1.xcoord;
+        float y1 = vertex1.ycoord;
+        float x2 = vertex2.xcoord;
+        float y2 = vertex2.ycoord;
+
+        double distance = calcDistanceBetweenPoints(x1, y1, x2, y2);
+
+        return Math.round(distance);
+    }
+
+    //calculate distance using pathagorean theorem
+    private double calcDistanceBetweenPoints(float x1, float y1, float x2, float y2) {
+        double aSquared = Math.pow((x2 - x1), 2);
+        double bSquared = Math.pow((y2 - y1), 2);
+        double cSquared = aSquared + bSquared;
+
+        return Math.sqrt(cSquared);
+    }
+
+    private void addEdgeToList(Vertex vertex1, Edge edge) {
+        vertex1.edges.add(edge);
     }
 
     protected void connectToFile() {
@@ -80,16 +109,6 @@ public class Graph {
         catch(FileNotFoundException ex) {
             System.out.print("***Could not connect to external data file***\n");
         }
-    }
-
-    //calculate distance between Euclidean 2D coordinates (i.e. edge weight)
-    protected long calcDistance(float x1, float y1, float x2, float y2){
-        double xTotal = Math.pow((x2 - x1), 2);
-        double yTotal = Math.pow((y2 - y1), 2);
-        double subTotal = xTotal + yTotal;
-        double result = Math.sqrt(subTotal);
-
-        return Math.round(result);
     }
 
     protected List<Vertex> getVertices(){
