6e5cad37010fb61c1e07a4afb3b85aa731cfd592
srubey <scrubey@pdx.edu>
2021-02-04 16:55:48 -0800

    Replaced vertex.xcoordinate and vertex.ycoordinate with Point class.  Each vertex now has a Location field implemented by the Point class.

--- src/Vertex.java
+++ src/Vertex.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 public class Vertex {
     protected String name;
-    protected float xcoord;
-    protected float ycoord;
+    protected Point location;
     protected Boolean visited;
     protected List<Edge> edges;
 
-    protected Vertex(String copyName, float copyX, float copyY){
+    protected Vertex(String copyName, Point copyLocation){
         this.name = copyName;
-        this.xcoord = copyX;
-        this.ycoord = copyY;
+        this.location = copyLocation;
         this.visited = false;
         this.edges = new ArrayList();
     }
